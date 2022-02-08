6b3f7df4dea223f1e228993ab87c6092e99510e3
Rodrigor0ver <you@example.com>
2022-02-08 20:17:37 -0300

    consulta a la api ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 
 function App() {
+
+  //State de la frase
+  const [frase, clickFrase] = useState({});
+
+  const consultaAPI = async() => {
+    const request = await fetch('https://los-simpsons-quotes.herokuapp.com/v1/quotes');
+    const result = await request.json();
+
+    clickFrase(result[0]);
+  };
+
+
+
 
 
   return (
     <div>
       <h1>Frases de los Simposons</h1>
       <section>Frase</section>
-      <button>La Jalea</button>
+      <button
+      onClick = {consultaAPI}
+      >La Jalea</button>
 
     </div>
 
